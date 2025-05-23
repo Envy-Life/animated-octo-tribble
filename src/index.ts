192e2a0a043fb35716c650c0f80693fffb093360
sahil pabale <dev.sahilpabale@gmail.com>
2025-05-23 14:26:19 +0530

    fix: bot obj export fixed

--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,28 @@
-import 'dotenv/config';
-import { drizzle } from 'drizzle-orm/node-postgres';
-import { eq } from 'drizzle-orm';
-import { Bot, MemorySessionStorage, session } from 'grammy';
-import { autoRetry } from '@grammyjs/auto-retry';
-import { run, sequentialize } from '@grammyjs/runner';
-import { conversations, createConversation } from '@grammyjs/conversations';
-import { newTradeController, newTradeConversation } from './controllers/new_trade';
-import * as schema from './db/schema';
-import { MyContext, MyConversationContext, type MySessionData } from './helpers/grammy';
-import { startController } from './controllers/start';
-import { Helius } from 'helius-sdk';
-import { seedDB } from './db/seed';
-import { migrateDB } from './db/seed';
+import "dotenv/config";
+import { drizzle } from "drizzle-orm/node-postgres";
+import { eq } from "drizzle-orm";
+import { Bot, MemorySessionStorage, session } from "grammy";
+import { autoRetry } from "@grammyjs/auto-retry";
+import { run, sequentialize } from "@grammyjs/runner";
+import { conversations, createConversation } from "@grammyjs/conversations";
+import {
+  newTradeController,
+  newTradeConversation,
+} from "./controllers/new_trade";
+import * as schema from "./db/schema";
+import {
+  MyContext,
+  MyConversationContext,
+  type MySessionData,
+} from "./helpers/grammy";
+import { startController } from "./controllers/start";
+import { Helius } from "helius-sdk";
+import { seedDB } from "./db/seed";
+import { migrateDB } from "./db/seed";
 
 const helius = new Helius(process.env.HELIUS_API_KEY!);
 
-process.on('uncaughtException', (err, origin) => {
+process.on("uncaughtException", (err, origin) => {
   console.error(err, origin);
 });
 
@@ -29,13 +36,13 @@ const db = drizzle(databaseUrl, {
   schema: schema,
 });
 
+const bot = new Bot<MyConversationContext>(token);
+
 (async () => {
   await migrateDB();
-  console.log('Migration completed');
+  console.log("Migration completed");
   await seedDB();
-  console.log('Seeding completed');
-
-  const bot = new Bot<MyConversationContext>(token);
+  console.log("Seeding completed");
 
   const initialSession = (): MySessionData => ({
     selectedMarketName: undefined,
@@ -54,7 +61,7 @@ const db = drizzle(databaseUrl, {
     session({
       initial: initialSession,
       storage: sessionStorage,
-    })
+    }),
   );
 
   bot.use(
@@ -62,7 +69,7 @@ const db = drizzle(databaseUrl, {
       const chat = ctx.chat?.id.toString();
       const user = ctx.from?.id.toString();
       return [chat, user].filter((con) => con !== undefined) as string[];
-    })
+    }),
   );
 
   bot.use(conversations());
@@ -76,12 +83,12 @@ const db = drizzle(databaseUrl, {
   await bot.api.setMyCommands([
     {
       command: "start",
-      description: "Setup your account or view home"
+      description: "Setup your account or view home",
     },
     {
       command: "trade",
-      description: "Start a new trade"
-    }
+      description: "Start a new trade",
+    },
   ]);
 
   process.once("SIGINT", () => bot.stop());
@@ -89,14 +96,16 @@ const db = drizzle(databaseUrl, {
 
   const handle = run(bot);
 
-  handle.task()?.then(() => {
-    console.log("Bot done processing!");
-  }).catch(err => {
-    console.error("Bot run handle error:", err);
-  });
+  handle
+    .task()
+    ?.then(() => {
+      console.log("Bot done processing!");
+    })
+    .catch((err) => {
+      console.error("Bot run handle error:", err);
+    });
 
   console.log("Bot is starting...");
-
 })();
 
-export { db, helius };
+export { bot, db, helius };
